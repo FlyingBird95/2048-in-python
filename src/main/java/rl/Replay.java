a4ad1113312f307d1fce0f382e4dd63f69e85ba9
flyingbird95 <patrickvogel@live.nl>
2017-12-21 17:09:22 +0100

    Added working DeepQ example

---
+++ src/main/java/rl/Replay.java
@@ -0,0 +1,22 @@
+package rl;
+
+import org.nd4j.linalg.api.ndarray.INDArray;
+
+public class Replay {
+
+	INDArray Input;
+
+	int Action; 
+	float Reward;
+	INDArray NextInput;
+	int NextActionMask[] ;
+	
+	Replay(INDArray input , int action , float reward , INDArray nextInput , int nextActionMask[]){
+		Input = input;
+		Action = action;
+		Reward = reward;
+		NextInput = nextInput;
+		NextActionMask = nextActionMask ;
+	}
+	
+}
