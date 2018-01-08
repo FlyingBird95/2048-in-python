d3210f2154c255d326c1d8aa57e0c91d4f1aef10
flyingbird95 <patrickvogel@live.nl>
2018-01-08 14:35:33 +0100

    Added board.py

--- python/py_2048_rl/learning/learning.py
+++ python/py_2048_rl/learning/learning.py
@@ -15,7 +15,6 @@ from py_2048_rl.learning.experience_batcher import ExperienceBatcher
 from py_2048_rl.learning.experience_collector import ExperienceCollector
 from py_2048_rl.learning.model import FeedModel
 
-
 STATE_NORMALIZE_FACTOR = 1.0 / 15.0
 
 
@@ -26,6 +25,7 @@ def make_run_inference(session, model):
     """Run inference on a given state_batch. Returns a q value batch."""
     return session.run(model.q_values,
                        feed_dict={model.state_batch_placeholder: state_batch})
+
   return run_inference
 
 
@@ -33,12 +33,14 @@ def make_get_q_values(session, model):
   """Make get_q_values() function for given session and model."""
 
   run_inference = make_run_inference(session, model)
+
   def get_q_values(state):
     """Run inference on a single (4, 4) state matrix."""
     state_vector = state.flatten() * STATE_NORMALIZE_FACTOR
     state_batch = np.array([state_vector])
     q_values_batch = run_inference(state_batch)
     return q_values_batch[0]
+
   return get_q_values
 
 
@@ -52,8 +54,8 @@ def run_training(train_dir):
     saver = tf.train.Saver(save_relative_paths=True)
     session = tf.Session()
     summary_writer = tf.summary.FileWriter(train_dir,
-                                            graph_def=session.graph_def,
-                                            flush_secs=10)
+                                           graph_def=session.graph_def,
+                                           flush_secs=10)
 
     if resume:
       print("Resuming: ", train_dir)
@@ -74,10 +76,10 @@ def run_training(train_dir):
     for state_batch, targets, actions in batcher.get_batches_stepwise():
 
       global_step, _ = session.run([model.global_step, model.train_op],
-          feed_dict={
-              model.state_batch_placeholder: state_batch,
-              model.targets_placeholder: targets,
-              model.actions_placeholder: actions,})
+                                   feed_dict={
+                                     model.state_batch_placeholder: state_batch,
+                                     model.targets_placeholder: targets,
+                                     model.actions_placeholder: actions, })
 
       if global_step % 10000 == 0 and global_step != 0:
         saver.save(session, train_dir + "/checkpoint", global_step=global_step)
@@ -90,14 +92,14 @@ def write_summaries(session, batcher, model, test_experiences, summary_writer):
   """Writes summaries by running the model on test_experiences. Returns loss."""
 
   state_batch, targets, actions = batcher.experiences_to_batches(
-      test_experiences)
+    test_experiences)
   state_batch_p, targets_p, actions_p = model.placeholders
   summary_str, global_step, loss = session.run(
-      [model.summary_op, model.global_step, model.loss],
-      feed_dict={
-          state_batch_p: state_batch,
-          targets_p: targets,
-          actions_p: actions,})
+    [model.summary_op, model.global_step, model.loss],
+    feed_dict={
+      state_batch_p: state_batch,
+      targets_p: targets,
+      actions_p: actions, })
   summary_writer.add_summary(summary_str, global_step)
   return loss
 
