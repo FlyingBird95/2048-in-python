afc75614ba433d573406a01c9f8582eed3bf3730
Tonnie Boersma <raket124@hotmail.com>
2018-01-09 22:25:59 +0100

    main

--- rl_2048/test.py
+++ rl_2048/test.py
@@ -3,3 +3,4 @@ from rl_2048.experience.ExperienceFactory import ExperienceFactory
 ef = ExperienceFactory('T') # change to your own letter, just so we can see who generated what
 
 for x in range(10): # generate 10 files, with 100000 entries each, just incase something goes wrong in the mean time
+    ef.generate('../experiments', 100000, False)
