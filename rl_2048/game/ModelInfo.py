b75726ff4b9c080aeeabad748f88dc4477d08463
Tonnie Boersma <raket124@hotmail.com>
2018-01-10 10:33:13 +0100

    small update

--- rl_2048/game/ModelInfo.py
+++ rl_2048/game/ModelInfo.py
@@ -13,30 +13,23 @@ class ModelInfo:
 
     @staticmethod
     def __is_move_possible(model):
-        for x in range(model.get_size()):
-            row = model.get_state_row(x)
-            for y in range(model.get_size() - 1):
-                if row[y] == row[y + 1]:
+        state = model.get_state()
+        for row in range(4):
+            has_empty = False
+            for col in range(4):
+                has_empty |= state[row, col] == 0
+                if state[row, col] != 0 and has_empty:
+                    return True
+                if state[row, col] != 0 and col > 0 and state[row, col] == state[row, col - 1]:
                     return True
         return False
 
     @staticmethod
     def get_possible_moves(model):
-        if model.__contains__(0):
-            return np.array([0, 1, 2, 3])
+        possible_moves = []
+        local_model = model.copy()
 
-        horizontal = ModelInfo.__is_move_possible(model)
-        model.rotate(1)
-        vertical = ModelInfo.__is_move_possible(model)
-        model.rotate(-1)
-
-        if vertical and horizontal:
-            return np.array([0, 1, 2, 3])
-
-        if vertical:
-            return np.array([0, 2])
-
-        if horizontal:
-            return np.array([1, 3])
-
-        return np.array([])
+        for x in range(4):
+            local_model.rotate(x)
+            if ModelInfo.__is_move_possible(local_model):
+                possible_moves.append(x)
