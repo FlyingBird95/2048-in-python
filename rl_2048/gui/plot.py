c97dd1197a71d1fa6f73398480e3b2ee3f90b190
flyingbird95 <patrickvogel@live.nl>
2018-01-18 14:05:10 +0100

    Updated plot

--- rl_2048/gui/plot.py
+++ rl_2048/gui/plot.py
@@ -1,10 +1,27 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 
-def show_plot(scores):
-    plt.plot(scores)
-    plt.ylabel('Simple plot')
-    plt.show()  # Doesn't work, but figure is correctly saved
-    plt.savefig('figure.png')
+def show_plot(scores, iteration=None):
+
+    y_mean = [np.mean(scores)] * len(scores)
+
+    fig, ax = plt.subplots()
+
+    # Plot the data
+    data_line = ax.plot(range(len(scores)), scores, label='Scores')
+
+    # Plot the average line
+    mean_line = ax.plot(range(len(scores)), y_mean, label='Mean', linestyle='--')
+
+    # Make a legend
+    legend = ax.legend(loc='upper right')
+
+    plt.show()
+    filename = 'figure'
+    if iteration:
+        filename += str(iteration)
+
+    plt.savefig(filename + '.png')
     print('Showing plot')
 
